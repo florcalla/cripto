a16388df04f65eb2499a17cbfde97e90eef28845
florcalla <62229132+florcalla@users.noreply.github.com>
2020-10-16 17:03:34 -0300

    Update cripto.js

--- cripto.js
+++ cripto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cripto
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Flxr
 // @require       https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.2/index.min.js
